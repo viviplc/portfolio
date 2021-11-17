754f2da3df761aeaf002d6d405869ac8ee5f897a
Viviana <vivi.plc95@hotmail.com>
2021-11-17 17:47:09 -0500

    capstone project complete missing data

--- routes/projects.js
+++ routes/projects.js
@@ -7,15 +7,15 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/capstone', function(req, res, next) {
-  res.render('projects/capstone', { title: 'Viviana Leyva Capstone project' });
+  res.render('projects/capstone', { title: 'Viviana Leyva Capstone project', project_title: 'Capstone' });
 });
 
 router.get('/bow', function(req, res, next) {
-  res.render('projects/bow', { title: 'Viviana Leyva BOW project' });
+  res.render('projects/bow', { title: 'Viviana Leyva BOW project', project_title: 'Bank of Waterloo' });
 });
 
 router.get('/lopawlty', function(req, res, next) {
-  res.render('projects/lopawlty', { title: 'Viviana Leyva lopawlty project' });
+  res.render('projects/lopawlty', { title: 'Viviana Leyva lopawlty project', project_title: 'Lopawlty' });
 });
 
 router.get('/tuTramite', function(req, res, next) {
