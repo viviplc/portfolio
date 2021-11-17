ee2e88de276e23d179cb6bc4a3b8c03574697223
Viviana <vivi.plc95@hotmail.com>
2021-11-17 10:12:21 -0500

    small fixes about page, projects routing complete

--- routes/projects.js
+++ routes/projects.js
@@ -3,11 +3,23 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('projects', { title: 'Home projects' });
+  res.render('projects', { title: 'Viviana Leyva Portfolio' });
 });
 
 router.get('/capstone', function(req, res, next) {
-  res.render('projects/capstone', { title: 'Capstone project' });
+  res.render('projects/capstone', { title: 'Viviana Leyva Capstone project' });
+});
+
+router.get('/bow', function(req, res, next) {
+  res.render('projects/bow', { title: 'Viviana Leyva BOW project' });
+});
+
+router.get('/lopawlty', function(req, res, next) {
+  res.render('projects/lopawlty', { title: 'Viviana Leyva lopawlty project' });
+});
+
+router.get('/tuTramite', function(req, res, next) {
+  res.render('projects/tuTramite', { title: 'Viviana Leyva tuTramite project' });
 });
 
 module.exports = router;
