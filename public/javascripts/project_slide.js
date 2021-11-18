10ac1ac47af930019fd1e0e73c9b0541a639af06
Viviana <vivi.plc95@hotmail.com>
2021-11-17 19:28:41 -0500

    commenting code from source reference

--- public/javascripts/project_slide.js
+++ public/javascripts/project_slide.js
@@ -13,16 +13,25 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
+  //reading elements from html
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
+
+  //limits validation for making a circular slider
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
+
+  //disable images from slides
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
+  //deactivate dots from view
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace("active", "");
   }
+
+  //showing slide selected
   slides[slideIndex-1].style.display = "block";
+  //activating dot related to the image
   dots[slideIndex-1].className += " active";
 }
